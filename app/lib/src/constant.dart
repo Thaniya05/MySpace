1ebc261ea7a08df05aec347d2b2b629f8ebd78a9
thaniya05 <thaniya14@gmail.com>
2023-04-06 14:02:59 +0700

    .

---
+++ app/lib/src/constant.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const String INTRO_ROLE = "Backend developer";
+const String INTRO_STATUS = "Interested and Studying Flutter";
+const List<IconData> INTRO_ICONS_LIST = [
+  FontAwesomeIcons.squareJs,
+  FontAwesomeIcons.html5,
+  FontAwesomeIcons.node,
+  FontAwesomeIcons.react
+];
+
+//aboutme.dart
+const String ABOUTME_DETAILS =
+    "มีความสนใจและตั้งใจที่จะเป็น Fronend และ Backend Developer โดยพร้อมที่จะเรียนรู้ Language programming และเครื่องมือใหม่ๆ ที่มีความน่าสนใจ หรือสามารถนำมาประยุกต์กับงานอยู่เสมอ ซึ่งสนใจเกี่ยวกับการทำ Web Application หรือ Mobile Application โดยตอน";
+
+const String ABOUTME_DETAIL =
+    "มีความถนัดทางด้านการใช้ Javascript , Typescript โดยจะทำในส่วนของการแสดงผลโดยใช้ HTML , React การดึงข้อมูล รวมไปถึงการสร้าง API ที่ใช้ในการติดต่อข้อมูลโดยใช้ NodeJS เป็นหลัก ซึ่งพร้อมจะศึกษาภาษา หรือเครื่องมือใหม่ๆอยู่เสมอ โดยตอนนี้กำลังสนใจการทำ Mobile Application รวมไปถึงการสร้าง Web โดยใช้ Flutter";
+
+//timeline.dart
+const String TIMELINE_JSON =
+    '[{"month" : "March" , "year" : "2023" , "details" : "Present"},{"month" : "August" ,"year":"2021","details":"Backend Developer , NewNN CO.,LTD"},{"month" : "October" ,"year":"2018","details":"Game Developer (intern) , NewNN CO.,LTD"},{"month" : "August" ,"year":"2017","details":"Computer Engineering , Khonkaen University"},{"month" : "March" ,"year":"2015","details":"Graduation from Khonkean Wittayayon"},{"month" : " " ,"year":"2010-2014","details":"Computer Olmypic ( POSN ) KKU"}]';
+
+//myexp.dart
+const String EXP_ROLE_1 = "Backend Developer";
+const String EXP_COMPANY_1 = "NewNN CO.,LTD";
+const String EXP_TIMELINE_1 = "Auguest 2021 - Present";
+const List<String> EXP_LIST_1 = [
+  "- ทำ Web Application โดยใช้ Javascript , Typescript  React ในส่วนของการแสดงผลทั้งหมดรวมถึง ฟังชั่นต่างๆ เช่น HTML to PDF , Autocomplete ",
+  "- ทำ API สำหรับการ CRUD และติดต่อ Database โดยใช้ NodeJS , Javascript ,Typescript",
+  "- ทำ Mobile Application โดยใช้ Blazor ( C# )",
+];
+
+const String EXP_ROLE_2 = "Backend Developer ( intern )";
+const String EXP_COMPANY_2 = "NewNN CO.,LTD";
+const String EXP_TIMELINE_2 = "October 2018 ";
+const List<String> EXP_LIST_2 = [
+  "- สร้าง Demo Game TurnBase 2 มิติ โดยใช้ Unreal engine 4 ",
+];
+
+const String EXP_ROLE_3 = "Freelance / Student";
+const String EXP_COMPANY_3 = "Khonkaen University";
+const String EXP_TIMELINE_3 = "2018-2020";
+const List<String> EXP_LIST_3 = [
+  "- [ Project ] ทำ Web Application ที่ติดต่อกับฐานข้อมูล โดยใช้ MongoDB",
+  "- [ Mini-Project] เกมโทรศัพท์ตอบคำถามภาษาไทยโดยใช้ Flutter ในการสร้าง",
+];
+
+//myskill.dart
+
+//myproject.dart
+
+const String MYPROJECT_DETAILS =
+    "ตัวอย่างผลงาน หรือ Project ที่กำลังทำ ที่สามารถนำมานำเสนอบน MySpace ได้ โดยถ้าต้องการติดต่อเกี่ยวกับงาน หรือ Resume ที่เป็นข้อมูลตัวเต็ม สามารถติดต่อได้ที่ Email : thaniya14@gmail.com";
+
+const String MYPROJECT_MYSPACE =
+    "MySpace เป็น Web Application ที่สร้างโดย Flutter ที่ทำขึ้นเพื่อฝึกฝนการใช้ Flutter รวมไปถึงเป็น Resume Online โดยในอนาคตจะพัฒนานำรายละเอียด ลูกเล่นต่างๆ  รวมไปถึงทำในส่วนของบทความมาลง และปรับปรุงแก้ไขใน Responsive ให้มีประสิทธิภาพมากยิ่งขึ้น";
+
+const String MYPROJECT_DEMOGAME =
+    "Game Demo ที่สร้างด้วย Unreal engine 4 โดยเป็นเกมต่อสู้แนว Turn-based โดยได้ทำในส่วนของระบบการต่อสู้ทั้งหมดรวมไปถึงการเชื่อมต่อ socket เพื่อต่อสู้ระหว่างผู้เล่นทั้งสองฝั่ง";
+
+const String MYPROJECT_WEBAPP =
+    "สร้าง Web Application กับทางบริษัท เช่น เว็บจำหน่ายสินค้า โดยใช้ในส่วนของ HTML , React ,Typescript หรือในบางโปรเจคก็เป็นทาง Blazor และ Asp.net";
+const String MYPROJECT_GAME24 =
+    "สร้าง Wiget เกม 24 ที่สามารถนำมาเล่นได้ทั้งบน Web และ Mobile";
+const String MYPROJECT_WIDGETTOPDF =
+    "Mini Project ที่จะสร้าง Widget ด้วย Flutter ที่มีลักษณะตกแต่งต่างๆแล้วนำมาแปลงเป็น PDF เพื่อใช้ในทำเป็น Resume ที่สามารถทำและแก้ไขออนไลน์";
---
+++ app/lib/src/widget/timeline.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:timelines/timelines.dart';
+import '../constant.dart' as constant;
+
+class MyTimeline extends StatelessWidget {
+  double timelineFontsize;
+  Color topicColor;
+  Color backgrondColor;
+  Color yearColor;
+  Color detailsColor;
+  MyTimeline(
+      {super.key,
+      this.timelineFontsize = 30.0,
+      this.topicColor = const Color(0xff53f6aa),
+      this.backgrondColor = const Color.fromARGB(255, 63, 113, 134),
+      this.yearColor = Colors.amber,
+      this.detailsColor = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    List<dynamic> list = jsonDecode(_json);
+
+    double widthsize = 0.4;
+    if (context.isPhone) {
+      widthsize = 0.8;
+    } else {
+      widthsize = 0.4;
+    }
+
+    return Container(
+        width: MediaQuery.of(context).size.width * widthsize,
+        child: Column(
+          children: [
+            Text(
+              "Timeline",
+              style: TextStyle(
+                color: topicColor,
+                fontSize: timelineFontsize,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            _buildBlank(),
+            Card(
+              elevation: 10,
+              color: backgrondColor,
+              child: Container(
+                width: MediaQuery.of(context).size.width * widthsize,
+                height: 500,
+                child: Timeline.tileBuilder(
+                  theme: TimelineThemeData(
+                      connectorTheme: ConnectorThemeData(
+                        color: topicColor,
+                      ),
+                      indicatorTheme: IndicatorThemeData(color: topicColor)),
+                  builder: TimelineTileBuilder.fromStyle(
+                    contentsAlign: ContentsAlign.alternating,
+                    contentsBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '${Timelineitem.fromJson(list[index]).month} ${Timelineitem.fromJson(list[index]).year}',
+                            style: TextStyle(color: yearColor),
+                          ),
+                          Center(
+                            child: Text(
+                              '${Timelineitem.fromJson(list[index]).details}',
+                              style: TextStyle(
+                                color: detailsColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    itemCount: list.length,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Padding _buildBlank([double d = 20.0]) {
+    return Padding(
+      padding: EdgeInsets.all(d),
+    );
+  }
+}
+
+class Timelineitem {
+  final String month;
+  final String year;
+  final String details;
+  final bool ispresent = false;
+
+  Timelineitem(this.month, this.year, this.details);
+
+  Timelineitem.fromJson(Map<String, dynamic> json)
+      : month = json['month'],
+        year = json['year'],
+        details = json['details'];
+
+  Map<String, dynamic> toJson() =>
+      {'name': month, 'year': year, 'details': details};
+}
+
+String _json = constant.TIMELINE_JSON;
