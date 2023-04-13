0d36aab8319050473e49c394d60a97077f1ad3c4
thaniya <thaniya14@gmail.com>
2023-04-13 11:32:24 +0700

    create routes , mainpage

--- app2/lib/myspace_module/views/homepage_view.dart
+++ app2/lib/myspace_module/views/homepage_view.dart
@@ -8,7 +8,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: const Text('Floating Nested SliverAppBar'),
+              floating: true,
+              expandedHeight: 100.0,
+              forceElevated: innerBoxIsScrolled,
+            ),
+          ];
+        },
+        body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: 30,
+          itemBuilder: (BuildContext context, int index) {
+            if (index % 2 == 0) {
+              return Container(
+                color: Colors.blue,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(child: Text('Item $index')),
+                ),
+              );
+            } else {
+              return Container(
+                color: Colors.amber,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(child: Text('Item $index')),
+                ),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
